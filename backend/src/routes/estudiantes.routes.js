d0b9d9afae48a6b342d8d3d349b41679be389f62
ValentinaM <valentinamart01@gmail.com>
2026-05-20 17:41:14 -0300

    Botón de paginación reparado

--- backend/src/routes/estudiantes.routes.js
+++ backend/src/routes/estudiantes.routes.js
@@ -13,6 +13,11 @@ router.post('/estudiantes', [
     check('email', 'No es un correo valido').isEmail(),
     validarCampos
 ], createEstudiante);
-router.put('/estudiantes/:id', updateEstudianteByID);
+router.put('/estudiantes/:id', [
+    check('nombres', 'El nombre es obligatorio').not().isEmpty(),
+    check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+    check('email', 'No es un correo valido').isEmail(),
+    validarCampos
+], updateEstudianteByID);
 router.delete('/estudiantes/:id', activarDesactivarEstudianteByID);
 export default router;
