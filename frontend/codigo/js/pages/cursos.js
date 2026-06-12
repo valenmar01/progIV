f06d3c0fe8301513ccbd4b6f077d6ad01fcb864b
segosal2002 <tomassalinas2002@gmail.com>
2026-06-12 13:29:56 -0300

    modificaciones en cursos

--- frontend/codigo/js/pages/cursos.js
+++ frontend/codigo/js/pages/cursos.js
@@ -1,19 +1,36 @@
 // esta función se encarga de cargar los cursos desde un archivo JSON, crear filas para cada curso y agregarlas a la tabla en el HTML. Si ocurre un error durante la carga, muestra un mensaje de error en la página.
+const API_URL = "http://localhost:3000/cursos";
+
+
 const cargarCursos = async () => {
     try {
-        const respuesta = await fetch("progIV\frontend\codigo\js\cursos.json");
+        const respuesta = await fetch(API_URL);
+
+
+        if (!respuesta.ok) {
+            throw new Error("Error al cargar los cursos, ${respuesta.status}");
+        }
+    
         const datos = await respuesta.json();
 
+
+        const listaCursos = json.cursos;
+
+
         const tabla = document.getElementById("tbody-cursos");
+
+
+        tabla.innerHTML = "";
+
         datos.forEach(curso => {
             const fila = document.createElement("tr");
             fila.innerHTML = `
-            <td>${curso.idCurso}</td>
+            <td>${curso.id_curso}</td>
             <td>${curso.nombre}</td>
             <td>${curso.descripcion}</td>
-            <td class="text-center">${new Date(curso.fechaInicio).toLocaleDateString()}</td>
-            <td class="text-center">${curso.cantidadHoras}</td>
-            <td class="text-center">${curso.inscriptosMax}</td>`;
+            <td class="text-center">${new Date(curso.fecha_inicio).toLocaleDateString()}</td>
+            <td class="text-center">${curso.cantidad_horas}</td>
+            <td class="text-center">${curso.inscriptos_max}</td>`;
             tabla.appendChild(fila);
         });
         document.getElementById("error").style.display = "none";
